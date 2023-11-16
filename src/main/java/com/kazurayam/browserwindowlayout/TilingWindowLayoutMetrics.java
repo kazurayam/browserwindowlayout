fbcb61412e8300710a13bc0d3aa0ba4df0cad8c6
kazurayam <kazuaki.urayama@gmail.com>
2023-11-16 16:17:59 +0900

    #3 TilingWindowLayoutMetrics class now has the columns property which defaults to 1

--- src/main/java/com/kazurayam/browserwindowlayout/TilingWindowLayoutMetrics.java
+++ src/main/java/com/kazurayam/browserwindowlayout/TilingWindowLayoutMetrics.java
@@ -12,8 +12,11 @@ public class TilingWindowLayoutMetrics extends WindowLayoutMetrics {
             new TilingWindowLayoutMetrics.Builder(8).build();
 
     private TilingWindowLayoutMetrics(Builder builder) {
+        this.columns = builder.columns;
         this.size = builder.size;
-        virtualScreenSize = new Dimension(builder.physicalScreenSize.getWidth() - builder.basePoint.getX() * 2, builder.physicalScreenSize.getHeight() - builder.basePoint.getY() * 2);
+        virtualScreenSize = new Dimension(
+                builder.physicalScreenSize.getWidth() - builder.basePoint.getX() * 2,
+                builder.physicalScreenSize.getHeight() - builder.basePoint.getY() * 2);
         basePoint = builder.basePoint;
     }
 
@@ -25,6 +28,8 @@ public class TilingWindowLayoutMetrics extends WindowLayoutMetrics {
         return basePoint;
     }
 
+    public int getColumns() { return this.columns; }
+
     @Override
     public int getSize() {
         return this.size;
@@ -33,19 +38,16 @@ public class TilingWindowLayoutMetrics extends WindowLayoutMetrics {
     @Override
     public Dimension getWindowDimension(final int windowIndex) {
         if (windowIndex < 0 || windowIndex >= this.size) {
-            throw new IllegalArgumentException("windowIndex=" + String.valueOf(windowIndex) + " must not be <0 and >=size");
+            throw new IllegalArgumentException("windowIndex=" + windowIndex + " must not be <0 and >=size");
         }
-
         if (size == 1) {
             return virtualScreenSize;
         } else {
-            // Tiles in 2 columns
-            int width = (int) Math.floor(virtualScreenSize.getWidth() / 2);
-            int rows = (int) Math.ceil(this.size / 2);
-            int height = (int) Math.floor(virtualScreenSize.getHeight() / rows);
+            int width = (int) Math.floor((double) virtualScreenSize.getWidth() / this.columns);
+            int rows = (int) Math.ceil((double) this.size / this.columns);
+            int height = (int) Math.floor((double) virtualScreenSize.getHeight() / rows);
             return new Dimension(width, height);
         }
-
     }
 
     @Override
@@ -54,9 +56,11 @@ public class TilingWindowLayoutMetrics extends WindowLayoutMetrics {
             throw new IllegalArgumentException("windowIndex=" +
                     String.valueOf(windowIndex) + " must not be <0 and >=size");
         }
+        int x = basePoint.getX() + (windowIndex % this.columns) * this.getWindowDimension(windowIndex).getWidth();
 
-        int x = basePoint.getX() + (windowIndex % 2) * this.getWindowDimension(windowIndex).getWidth();
-        int y = (int) ((int) basePoint.getY() + Math.floor(windowIndex / 2) * this.getWindowDimension(windowIndex).getHeight());
+        int y = (int) ((int) basePoint.getY() +
+                Math.floor((double) windowIndex / this.columns) * this.getWindowDimension(windowIndex).getHeight());
+
         return new Point(x, y);
     }
 
@@ -71,7 +75,8 @@ public class TilingWindowLayoutMetrics extends WindowLayoutMetrics {
         }
 
         TilingWindowLayoutMetrics other = (TilingWindowLayoutMetrics) o;
-        return this.virtualScreenSize.equals(other.getVirtualScreenSize()) && this.basePoint.equals(other.getBasePoint());
+        return this.virtualScreenSize.equals(other.getVirtualScreenSize()) &&
+                this.basePoint.equals(other.getBasePoint());
     }
 
     @Override
@@ -86,6 +91,7 @@ public class TilingWindowLayoutMetrics extends WindowLayoutMetrics {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("{\"TilingWindowLayoutMetrics\":{");
+        sb.append("\"columns\":" + getColumns());
         sb.append("\"size\":" + getSize());
         sb.append(",");
         sb.append("\"virtualScreenSize\":[" + String.valueOf(getVirtualScreenSize().getWidth()) + "," + String.valueOf(getVirtualScreenSize().getHeight()) + "]");
@@ -95,6 +101,7 @@ public class TilingWindowLayoutMetrics extends WindowLayoutMetrics {
         return sb.toString();
     }
 
+    private final int columns;
     private final int size;
     private final Dimension virtualScreenSize;
     private final Point basePoint;
@@ -103,12 +110,19 @@ public class TilingWindowLayoutMetrics extends WindowLayoutMetrics {
      * Builder pattern by "Effective Java"
      */
     public static class Builder {
-        public Builder(final int size) {
+        public Builder(final int columns, final int size) {
+            if (size <= 0) {
+                throw new IllegalArgumentException("columns=" + String.valueOf(columns) + " must not be <=0");
+            }
             if (size <= 0) {
                 throw new IllegalArgumentException("size=" + String.valueOf(size) + " must not be <=0");
             }
-
+            this.columns = columns;
             this.size = size;
+        }
+
+        public Builder(final int size) {
+            this(1, size);
         }
 
         public Builder physicalScreenSize(Dimension physicalScreenSize) {
@@ -126,6 +140,7 @@ public class TilingWindowLayoutMetrics extends WindowLayoutMetrics {
         }
 
         private int size;
+        private int columns;
         private java.awt.Dimension ss = Toolkit.getDefaultToolkit().getScreenSize();
         private Dimension physicalScreenSize = new Dimension((int) ss.getWidth(), (int) ss.getHeight());
         private Point basePoint = new Point(10, 10);
--- src/test/groovy/com/kazurayam/browserwindowlayout/TilingWindowLayoutMetricsTest.groovy
+++ src/test/groovy/com/kazurayam/browserwindowlayout/TilingWindowLayoutMetricsTest.groovy
@@ -2,7 +2,7 @@ package com.kazurayam.browserwindowlayout
 
 import static org.junit.Assert.*
 
-import org.openqa.selenium.Dimension as Dimension
+import org.openqa.selenium.Dimension
 import org.openqa.selenium.Point
 import org.junit.Before
 import org.junit.Test
@@ -12,70 +12,112 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4.class)
 class TilingWindowLayoutMetricsTest {
 
-    TilingWindowLayoutMetrics layoutMetrics
+    TilingWindowLayoutMetrics tilesSingleColumn
+    TilingWindowLayoutMetrics tilesDualColumns
     int NUM_OF_WINDOWS = 4
 
     @Before
     void setup() {
-        layoutMetrics = new TilingWindowLayoutMetrics.Builder(NUM_OF_WINDOWS)
+        // single column
+        tilesSingleColumn = new TilingWindowLayoutMetrics.Builder(NUM_OF_WINDOWS)
+                .physicalScreenSize(new Dimension(1020, 820)).build()
+
+        // 2 columns
+        tilesDualColumns = new TilingWindowLayoutMetrics.Builder(2, NUM_OF_WINDOWS)
                 .physicalScreenSize(new Dimension(1020, 820)).build()
     }
 
     @Test
     void test_getVirtualScreenSize() {
-        Dimension virtualScreenSize = layoutMetrics.getVirtualScreenSize()
+        Dimension virtualScreenSize = tilesSingleColumn.getVirtualScreenSize()
         assertTrue(virtualScreenSize.width == 1000)
         assertTrue(virtualScreenSize.height == 800)
     }
 
     @Test
     void test_getBasePoint() {
-        Point basePoint = layoutMetrics.getBasePoint()
+        Point basePoint = tilesSingleColumn.getBasePoint()
         assertTrue(basePoint.x == 10)
         assertTrue(basePoint.y == 10)
     }
 
     @Test
-    void test_getWindowDimension() {
-        Dimension tileDimension = layoutMetrics.getWindowDimension(0)
-        assertTrue("expected width == 500", tileDimension.width == 500)
-        assertTrue("expected height == 400", tileDimension.height == 400)
+    void test_getWindowDimension_1column() {
+        Dimension tileDimension = tilesSingleColumn.getWindowDimension(0)
+        assertEquals("tileDimension.width", 1000, tileDimension.width)
+        assertEquals("tileDimension.height", 200, tileDimension.height)
     }
 
     @Test
-    void testWindow0() {
-        Point basePoint = layoutMetrics.getBasePoint()
-        Point loc = layoutMetrics.getWindowPosition(0)
-        Dimension dim = layoutMetrics.getWindowDimension(0)
+    void test_singleColumn_window0() {
+        Point basePoint = tilesSingleColumn.getBasePoint()
+        Point loc = tilesSingleColumn.getWindowPosition(0)
+        Dimension dim = tilesSingleColumn.getWindowDimension(0)
         Point expectedLoc = new Point((int)basePoint.x, (int)basePoint.y)
-        assertTrue("loc.x=${loc.x}, expected to be ${expectedLoc.x}", expectedLoc.x == loc.x)
-        assertTrue("loc.y=${loc.y}, expected to be ${expectedLoc.y}", expectedLoc.y == loc.y)
-        assertTrue("dim.width=${dim.width}, expected to be 500", 500 == dim.width)
-        assertTrue("dim.height=${dim.height}, expected to be 400", 400 == dim.height)
+        assertEquals("loc.x", expectedLoc.x, loc.x)
+        assertEquals("loc.y", expectedLoc.y, loc.y)
+        assertEquals("dim.width", 1000, dim.width)
+        assertEquals("dim.height", 200, dim.height)
     }
 
     @Test
-    void testWindow1() {
-        Point basePoint = layoutMetrics.getBasePoint()
-        Point loc = layoutMetrics.getWindowPosition(1)
-        Dimension dim = layoutMetrics.getWindowDimension(1)
+    void test_singleColumn_window1() {
+        Point basePoint = tilesSingleColumn.getBasePoint()
+        Point loc = tilesSingleColumn.getWindowPosition(1)
+        Dimension dim = tilesSingleColumn.getWindowDimension(1)
+        Point expectedLoc = new Point((int)basePoint.x, (int)basePoint.y + (int)dim.getHeight())
+        assertEquals("loc.x", expectedLoc.x, loc.x)
+        assertEquals("loc.y", expectedLoc.y, loc.y)
+        assertEquals("dim.width", 1000, dim.width)
+        assertEquals("dim.height", 200, dim.height)
+    }
+
+    @Test
+    void test_singleColumn_window2() {
+        Point basePoint = tilesSingleColumn.getBasePoint()
+        Point loc = tilesSingleColumn.getWindowPosition(2)
+        Dimension dim = tilesSingleColumn.getWindowDimension(2)
+        Point expectedLoc = new Point((int)basePoint.x, (int)basePoint.y + (int)dim.height * 2)
+        assertEquals("loc.x", expectedLoc.x, loc.x)
+        assertEquals("loc.y", expectedLoc.y, loc.y)
+        assertEquals("dim.width", 1000, dim.width)
+        assertEquals("dim.height", 200, dim.height)
+    }
+
+    @Test
+    void test_dualColumns_window0() {
+        Point basePoint = tilesDualColumns.getBasePoint()
+        Point loc = tilesDualColumns.getWindowPosition(0)
+        Dimension dim = tilesDualColumns.getWindowDimension(0)
+        Point expectedLoc = new Point((int)basePoint.x, (int)basePoint.y)
+        assertEquals("loc.x", expectedLoc.x, loc.x)
+        assertEquals("loc.y", expectedLoc.y, loc.y)
+        assertEquals("dim.width", 500, dim.width)
+        assertEquals("dim.height", 400, dim.height)
+    }
+
+    @Test
+    void test_dualColumns_window1() {
+        Point basePoint = tilesDualColumns.getBasePoint()
+        Point loc = tilesDualColumns.getWindowPosition(1)
+        Dimension dim = tilesDualColumns.getWindowDimension(1)
         Point expectedLoc = new Point((int)basePoint.x + (int)dim.width, (int)basePoint.y)
-        assertTrue("loc.x=${loc.x}, expected to be ${expectedLoc.x}", expectedLoc.x == loc.x)
-        assertTrue("loc.y=${loc.y}, expected to be ${expectedLoc.y}", expectedLoc.y == loc.y)
-        assertTrue("dim.width=${dim.width}, expected to be 500", 500 == dim.width)
-        assertTrue("dim.height=${dim.height}, expected to be 400", 400 == dim.height)
+        assertEquals("loc.x", expectedLoc.x, loc.x)
+        assertEquals("loc.y", expectedLoc.y, loc.y)
+        assertEquals("dim.width", 500, dim.width)
+        assertEquals("dim.height", 400, dim.height)
     }
 
     @Test
-    void testWindow2() {
-        Point basePoint = layoutMetrics.getBasePoint()
-        Point loc = layoutMetrics.getWindowPosition(2)
-        Dimension dim = layoutMetrics.getWindowDimension(2)
+    void test_dualColumns_window2() {
+        Point basePoint = tilesDualColumns.getBasePoint()
+        Point loc = tilesDualColumns.getWindowPosition(2)
+        Dimension dim = tilesDualColumns.getWindowDimension(2)
         Point expectedLoc = new Point((int)basePoint.x, (int)basePoint.y + (int)dim.height)
-        assertTrue("loc.x=${loc.x}, expected to be ${expectedLoc.x}", expectedLoc.x == loc.x)
-        assertTrue("loc.y=${loc.y}, expected to be ${expectedLoc.y}", expectedLoc.y == loc.y)
-        assertTrue("dim.width=${dim.width}, expected to be 500", 500 == dim.width)
-        assertTrue("dim.height=${dim.height}, expected to be 400", 400 == dim.height)
+        assertEquals("loc.x", expectedLoc.x, loc.x)
+        assertEquals("loc.y", expectedLoc.y, loc.y)
+        assertEquals("dim.width", 500, dim.width)
+        assertEquals("dim.height", 400, dim.height)
     }
 
 }
